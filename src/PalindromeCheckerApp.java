c89b1e8cc47b51088cd66d9753958e2960071cd8
neel_597 <neelaka597@gmail.com>
2026-02-25 10:55:59 +0530

    UC6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,32 @@
-public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to Palindrome App Checker\nVersion 1.0");
+ void main() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=== UC6: Queue + Stack Based Palindrome Checker ===");
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        input = input.replaceAll("\\s+","").toLowerCase();
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+        // Enqueue and Push characters
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            queue.add(ch);      // Enqueue (FIFO)
+            stack.push(ch);     // Push (LIFO)
+        }
+        boolean isPalindrome = true;
+        // Compare dequeue and pop
+        while (!queue.isEmpty()) {
+            char fromQueue = queue.remove();  // Dequeue
+            char fromStack = stack.pop();     // Pop
+
+            if (fromQueue != fromStack) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("Result: The given string is a Palindrome.");
+        } else {
+            System.out.println("Result: The given string is NOT a Palindrome.");
+        }
+        scanner.close();
     }
-}
